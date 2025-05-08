27736cca1a2722dc417476c1bc6c2dfb4e7902ab
radz22 <radzsantillan01@gmail.com>
2025-05-08 12:08:39 +0800

    change

--- src/gameusers/Homegame.jsx
+++ src/gameusers/Homegame.jsx
@@ -69,7 +69,11 @@ const Homegame = () => {
         <div className="flex justify-center items-center mt-10">
           <Link to="/lesson">
             <div
-              className="flex items-center justify-center bg-[#1792f7] font-bold text-black text-xl hover:bg-[#44abff] hover:scale-110 transition-transform duration-300 rounded-full w-[200px] h-[70px] p-5 cursor-pointer focus:outline-none focus:ring-2 focus:ring-[#00e5ff] drop-shadow-[0_0_10px_rgba(0,229,255,0.7)]"
+              className="flex items-center justify-center bg-[#1792f7] font-bold text-black text-xl hover:bg-[#44abff] hover:scale-110 transition-transform duration-300 rounded-full w-[200px] h-[70px] p-5 cursor-pointer focus:outline-none focus:ring-2 focus:ring-[#00e5ff] drop-shadow-[0_0_10px_rgba(0,229,255,0.7)]
+              
+              max-xl:w-[200px] max-lg:w-[180px] max-md:w-[160px] max-sm:w-[140px]
+              max-xl:h-[70px] max-lg:h-[60px] max-md:h-[50px] max-sm:h-[40px]
+               max-xl:text-xl max-lg:text-lg max-md:text-md max-sm:text-sm"
               style={{
                 boxShadow: `
                   0 0 10px rgba(255, 255, 255, 0.8), 
